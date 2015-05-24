cffcc158cadb2a8f1773545905a4a665204ff9e9
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-24 08:40:27 -0400

    initial commit

---
+++ PlayingWithSoundCloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayingWithSoundCloud
+//
+//  Created by Michael Vilabrera on 5/24/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
